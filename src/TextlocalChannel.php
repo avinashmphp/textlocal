4875ad723b8bf5090695a64f800f44f37aa2e6c6
Avinash Mohammed Haneefa <avinash@dopravo.com>
2019-04-04 10:14:27 +0300

    Channel error fixed

--- src/TextlocalChannel.php
+++ src/TextlocalChannel.php
@@ -13,7 +13,6 @@ class TextlocalChannel extends Textlocal
     {
         parent::__construct('', '', config('textlocal.key'));
         $this->sender = config('textlocal.sender');
-      
     }
 
     public function send($notifiable, Notification $notification)
@@ -21,10 +20,18 @@ class TextlocalChannel extends Textlocal
         if (! $numbers = $notifiable->routeNotificationFor('textlocal')) {
             return;
         }
+        if (empty($numbers)) {
+            return;
+        }
+        if (!is_array($numbers)) {
+            $numbers = [$numbers];
+        }
         $message = $notification->toTextlocal($notifiable);
+        if (empty($message)) {
+            return;
+        }
          $resutls = $this->sendSms($numbers, $message, $this->sender);
          //    dd($resutls);
          return $resutls;
     }
-      
 }
